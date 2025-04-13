c0d3bdb7e50737c21701da39b2a373fd406f79b2
Pradeep <pk4401128@gmail.com>
2025-04-13 14:36:19 +0530

    refactor: restore and organize imports in HomePage and ServicesPage components

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,43 +1,31 @@
-// import React from 'react'
-// import Component16 from '../preComponent/Component16'
-// import Component15 from '../preComponent/Component15'
-// import Component14 from '../preComponent/Component14'
-// import Component6 from '../preComponent/Component6'
-// import Component11 from '../preComponent/Component11'
-// import Component13 from '../preComponent/Component13'
-// import Component10 from '../components/Component10'
-// import Component7 from '../components/Component7'
-// import Component8 from '../components/Component8'
-// import Component9 from '../components/Component9'
-
-// const HomePage = () => {
-//   return (
-//     <div>
-//       <Component15 />
-//       <Component14 />
-//       <Component9 />
-//       <Component6 />
-//       <Component11 />
-//       <Component13 />
-//       <Component10 />
-//       <Component7 />
-//       <Component8 />
-//     </div>
-//   )
-// }
-
-// export default HomePage
-
-
-
 import React from 'react'
+import Component15 from '../preComponent/Component15'
+import Component14 from '../preComponent/Component14'
+import Component9 from '../components/Component9'
+import Component6 from '../preComponent/Component6'
+import Component11 from '../preComponent/Component11'
+import Component13 from '../preComponent/Component13'
+import Component10 from '../components/Component10'
+import Component7 from '../components/Component7'
+import Component8 from '../components/Component8'
+
+
 
 const HomePage = () => {
   return (
     <div>
-      home Pages
+      <Component15 />
+      <Component14 />
+      <Component9 />
+      <Component6 />
+      <Component11 />
+      <Component13 />
+      <Component10 />
+      <Component7 />
+      <Component8 />
     </div>
   )
 }
 
 export default HomePage
+
--- src/Pages/ServicesPage.jsx
+++ src/Pages/ServicesPage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Component1 from '../components/Component1'
+import Component2 from '../components/Component2'
+import Component10 from '../components/Component10'
+import Component11 from '../preComponent/Component11'
+import Component7 from '../components/Component7'
+import Component8 from '../components/Component8'
 
 const ServicePage = () => {
   return (
-    <div>
-      welcome to service page
-    </div>
+    <>
+      <Component1 />
+      <Component2 />
+      <Component10 />
+      <Component11 />
+      <Component7 />
+      <Component8 />
+
+    </>
+
   )
 }
 
