d1612427d6b491234f7cc9bfdb593bed4b83bffb
Pradeep <pk4401128@gmail.com>
2025-04-13 14:19:27 +0530

    refactor: clean up imports and simplify ServicesPage component

--- src/Pages/Diagnostics.jsx
+++ src/Pages/Diagnostics.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Component4 from '../preComponent/component4';
 import Component17 from '../components/component17';
 import Component12 from '../components/component12';
-import Component10 from '../components/component10';
 import Component11 from '../preComponent/component11';
 import Component7 from '../components/component7';
 import Component8 from '../components/component8';
 import Component_10 from '../components/component_10';
+import Component10 from '../components/component10';
+import Component19 from '../components/Component19';
+import Component3 from '../components/Component3';
 
 const Diagnostics = () => {
   return (
@@ -15,6 +17,8 @@ const Diagnostics = () => {
         <Component17 />
         <Component12 />
         <Component10 />
+        <Component3 />
+        <Component19 />
         <Component_10 />
         <Component11 />
         <Component7 />
--- src/Pages/ServicesPage.jsx
+++ src/Pages/ServicesPage.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
-import Component1 from '../components/Component1'
-import Component2 from '../components/Component2'
-import Component10 from '../components/component10'
-import Component11 from '../preComponent/component11'
-import Component7 from '../components/component7'
-import Component8 from '../components/component8'
 
-const ServicesPage = () => {
+const ServicePage = () => {
   return (
     <div>
-      <Component1 />
-      <Component2 />
-      <Component10 />
-      <Component11 />
-      <Component7 />
-      <Component8 />
+      welcome to service page
     </div>
   )
 }
 
-export default ServicesPage
+export default ServicePage
