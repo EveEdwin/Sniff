d7d3313895838def29c4352e804855275702854f
Ramkishan Gupta <145246814+Ramkishangupta@users.noreply.github.com>
2025-05-20 20:25:38 +0530

    feat: Add Axios

--- src/api.js
+++ src/api.js
@@ -8,16 +8,16 @@ export const fetchFeatures = async () => {
 };
 
 export const fetchSuccessStories = async () => {
-  const response = await axios.get(`${BASE_URL}/success-stories`);
+  const response = await axios.get(`${BASE_URL}/api/success-stories`);
   return response.data;
 };
 
 export const fetchEvents = async () => {
-  const response = await axios.get(`${BASE_URL}/events`);
+  const response = await axios.get(`${BASE_URL}/api/events`);
   return response.data;
 };
 
 export const fetchCommunityPosts = async () => {
-  const response = await axios.get(`${BASE_URL}/community`);
+  const response = await axios.get(`${BASE_URL}/api/community`);
   return response.data;
 };
